f54adc7efaba194540864ff4cdfce55526a8c333
ExPHAT <me@exphat.com>
2015-10-02 20:37:52 -0400

    added a few tests

--- test/index.js
+++ test/index.js
@@ -1,4 +1,18 @@
 var expect = require("expect.js");
+var compiless = require("../");
 
 describe("compiless", function() {
+  it("should return a function", function() {
+    expect(compiless).to.be.a(Function);
+  });
+
+  it("should throw an error if no arguments are given", function() {
+    try {
+      compiless();
+      expect().fail("Did not throw an error");
+    }
+    catch (err) {
+      expect(err).to.be.an(Error);
+    }
+  });
 });
